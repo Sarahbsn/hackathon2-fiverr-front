40477aae5bd0e6002075893f24307d5235efcd24
TouyardK <touyard.kevin@outlook.fr>
2021-06-28 20:49:55 +0200

    Creation du bouton generique

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      Hackathon 2 - Fiverr Oh yeaaah
-    </div>
-  );
+  return <div className="App">Hackathon 2 - Fiverr Oh yeaaah</div>;
 }
 
 export default App;
