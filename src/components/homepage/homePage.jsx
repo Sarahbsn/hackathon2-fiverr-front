05b25c30c75fe779e9d7c850621310438bac6e13
TouyardK <touyard.kevin@outlook.fr>
2021-06-30 10:46:55 +0200

    Maj routes

--- src/components/homepage/homePage.jsx
+++ src/components/homepage/homePage.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Couple from '../../assets/couple.png';
 import Card from '../../assets/Card2.png';
 import {
@@ -21,7 +22,9 @@ const Homepage = () => {
         <StyledLeftContainer>
           <StyledTitle1>Welcome to the Fiverr Community</StyledTitle1>
           <StyledParagraphe>Get access to upcoming events, exclusive content, the latest and greatest industry news, and more.</StyledParagraphe>
-          <StyledButton style={{width:'20%', fontSize:'25px'}}>Sign in</StyledButton>
+          <Link to="/Connection">
+            <StyledButton style={{ width: '100%', fontSize: '25px' }}>Sign in</StyledButton>
+          </Link>
         </StyledLeftContainer>
         <StyledRightContainer>
           <StyledImage src={Couple} alt="bureaux" />
